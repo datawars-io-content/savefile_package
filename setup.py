d9882da08e2dfc1bc0687440a93285651d8c9837
anurag629 <akvermaav629@gmail.com>
2025-03-03 20:10:00 +0530

    added all

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="savefile",
-    version="0.1",
-    packages=find_packages(),
-    install_requires=["numpy", "pandas", "matplotlib"],
-    author="Anurag Verma",
-    description="A utility package to save different file formats, including images, and zip them.",
+    name="savefile",  # Package name (unique on PyPI)
+    version="0.1.0",  # Version number (increment on updates)
+    packages=find_packages(),  # Automatically detect package modules
+    install_requires=["numpy", "pandas", "matplotlib"],  # Dependencies
+    author="Anurag Verma", 
+    description="A utility package to save various data formats and zip them.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/anurag629/savefile",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
 )
